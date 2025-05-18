3acffb4a71f07a0675c36166b9bf93e46ed12a30
anthonhassel <anthon.hassel@gmail.com>
2025-05-18 23:29:25 +0200

    docs: remove example order dictionary from create_order function docstring

--- server.py
+++ server.py
@@ -170,54 +170,6 @@ def retrieve_reviews_totals():
 def create_order(order: dict):
     """
     Create an order in WooCommerce.
-
-    Example of the expected 'order' dictionary:
-
-        data = {
-            "payment_method": "bacs",
-            "payment_method_title": "Direct Bank Transfer",
-            "set_paid": True,
-            "billing": {
-                "first_name": "John",
-                "last_name": "Doe",
-                "address_1": "969 Market",
-                "address_2": "",
-                "city": "San Francisco",
-                "state": "CA",
-                "postcode": "94103",
-                "country": "US",
-                "email": "john.doe@example.com",
-                "phone": "(555) 555-5555"
-            },
-            "shipping": {
-                "first_name": "John",
-                "last_name": "Doe",
-                "address_1": "969 Market",
-                "address_2": "",
-                "city": "San Francisco",
-                "state": "CA",
-                "postcode": "94103",
-                "country": "US"
-            },
-            "line_items": [
-                {
-                    "product_id": 93,
-                    "quantity": 2
-                },
-                {
-                    "product_id": 22,
-                    "variation_id": 23,
-                    "quantity": 1
-                }
-            ],
-            "shipping_lines": [
-                {
-                    "method_id": "flat_rate",
-                    "method_title": "Flat Rate",
-                    "total": "10.00"
-                }
-            ]
-        }
     """
     response = wcapi.post("orders", order)
     return str(response.json())
